2b1b23a951962c60675ec4ad11dd1b45f8cbc4bd
Akatosh <romaspost4work@gmail.com>
2019-11-25 16:59:03 +0200

    'ready'

--- convertor.py
+++ convertor.py
@@ -1,4 +1,6 @@
 from PIL import ImageTk, Image, ImageFilter
+import cv2
+import os
 
 
 class Convertor:
@@ -30,6 +32,160 @@ class Convertor:
 
     def blur(self, radius):
         self.pilImg = self.pilImg.filter(ImageFilter.GaussianBlur(radius=radius))
+        if self.pilImg.size[0] >= self.pilImg.size[1]:
+            size = (700, int(self.pilImg.size[1] / (self.pilImg.size[0] / 700)))
+        else:
+            size = (int(self.pilImg.size[0] / (self.pilImg.size[1] / 400)), 400)
+        self.tkImg = ImageTk.PhotoImage(self.pilImg.resize(size, Image.ANTIALIAS))
+
+    def detection(self):
+        cascade_path = os.path.join(os.path.dirname(__file__), 'haarcascade_frontalface_default.xml')
+        face_cascade = cv2.CascadeClassifier(cascade_path)
+        image = cv2.imread(self.path)
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(
+            gray,
+            scaleFactor=1.1,
+            minNeighbors=5,
+            minSize=(10, 10)
+        )
+        faces_detected = "Лиц обнаружено: " + format(len(faces))
+        for (x, y, w, h) in faces:
+            cv2.rectangle(image, (x, y), (x+w, y+h), (255, 255, 0), 2)
+        b,g,r = cv2.split(image)
+        image = cv2.merge((r,g,b))
+        self.pilImg = Image.fromarray(image)
+        if self.pilImg.size[0] >= self.pilImg.size[1]:
+            size = (700, int(self.pilImg.size[1] / (self.pilImg.size[0] / 700)))
+        else:
+            size = (int(self.pilImg.size[0] / (self.pilImg.size[1] / 400)), 400)
+        self.tkImg = ImageTk.PhotoImage(self.pilImg.resize(size, Image.ANTIALIAS))
+
+    def contour(self):
+        self.pilImg = self.pilImg.filter(ImageFilter.CONTOUR)
+        if self.pilImg.size[0] >= self.pilImg.size[1]:
+            size = (700, int(self.pilImg.size[1] / (self.pilImg.size[0] / 700)))
+        else:
+            size = (int(self.pilImg.size[0] / (self.pilImg.size[1] / 400)), 400)
+        self.tkImg = ImageTk.PhotoImage(self.pilImg.resize(size, Image.ANTIALIAS))
+
+    def brightness(self, brightness):
+        result = Image.new('RGB', self.pilImg.size)
+        for x in range(self.pilImg.size[0]):
+            for y in range(self.pilImg.size[1]):
+                r, g, b = self.pilImg.getpixel((x, y))
+
+                red = int(r * brightness)
+                red = min(255, max(0, red))
+
+                green = int(g * brightness)
+                green = min(255, max(0, green))
+
+                blue = int(b * brightness)
+                blue = min(255, max(0, blue))
+
+                result.putpixel((x, y), (red, green, blue))
+
+        self.pilImg = result
+        if self.pilImg.size[0] >= self.pilImg.size[1]:
+            size = (700, int(self.pilImg.size[1] / (self.pilImg.size[0] / 700)))
+        else:
+            size = (int(self.pilImg.size[0] / (self.pilImg.size[1] / 400)), 400)
+        self.tkImg = ImageTk.PhotoImage(self.pilImg.resize(size, Image.ANTIALIAS))
+
+    def negative(self):
+        result = Image.new('RGB', self.pilImg.size)
+        for x in range(self.pilImg.size[0]):
+            for y in range(self.pilImg.size[1]):
+                r, g, b = self.pilImg.getpixel((x, y))
+                result.putpixel((x, y), (255 - r, 255 - g, 255 - b))
+        self.pilImg = result
+        if self.pilImg.size[0] >= self.pilImg.size[1]:
+            size = (700, int(self.pilImg.size[1] / (self.pilImg.size[0] / 700)))
+        else:
+            size = (int(self.pilImg.size[0] / (self.pilImg.size[1] / 400)), 400)
+        self.tkImg = ImageTk.PhotoImage(self.pilImg.resize(size, Image.ANTIALIAS))
+
+    def white_black(self, brightness):
+        result = Image.new('RGB', self.pilImg.size)
+        separator = 255 / brightness / 2 * 3
+        for x in range(self.pilImg.size[0]):
+            for y in range(self.pilImg.size[1]):
+                r, g, b = self.pilImg.getpixel((x, y))
+                total = r + g + b
+                if total > separator:
+                    result.putpixel((x, y), (255, 255, 255))
+                else:
+                    result.putpixel((x, y), (0, 0, 0))
+        self.pilImg = result
+        if self.pilImg.size[0] >= self.pilImg.size[1]:
+            size = (700, int(self.pilImg.size[1] / (self.pilImg.size[0] / 700)))
+        else:
+            size = (int(self.pilImg.size[0] / (self.pilImg.size[1] / 400)), 400)
+        self.tkImg = ImageTk.PhotoImage(self.pilImg.resize(size, Image.ANTIALIAS))
+
+    def grayscale(self):
+        result = Image.new('RGB', self.pilImg.size)
+        for x in range(self.pilImg.size[0]):
+            for y in range(self.pilImg.size[1]):
+                r, g, b = self.pilImg.getpixel((x, y))
+                gray = int(r * 0.2126 + g * 0.7152 + b * 0.0722)
+                result.putpixel((x, y), (gray, gray, gray))
+        self.pilImg = result
+        if self.pilImg.size[0] >= self.pilImg.size[1]:
+            size = (700, int(self.pilImg.size[1] / (self.pilImg.size[0] / 700)))
+        else:
+            size = (int(self.pilImg.size[0] / (self.pilImg.size[1] / 400)), 400)
+        self.tkImg = ImageTk.PhotoImage(self.pilImg.resize(size, Image.ANTIALIAS))
+
+    def sepia(self):
+        result = Image.new('RGB', self.pilImg.size)
+        for x in range(self.pilImg.size[0]):
+            for y in range(self.pilImg.size[1]):
+                r, g, b = self.pilImg.getpixel((x, y))
+                red = int(r * 0.393 + g * 0.769 + b * 0.189)
+                green = int(r * 0.349 + g * 0.686 + b * 0.168)
+                blue = int(r * 0.272 + g * 0.534 + b * 0.131)
+                result.putpixel((x, y), (red, green, blue))
+        self.pilImg = result
+        if self.pilImg.size[0] >= self.pilImg.size[1]:
+            size = (700, int(self.pilImg.size[1] / (self.pilImg.size[0] / 700)))
+        else:
+            size = (int(self.pilImg.size[0] / (self.pilImg.size[1] / 400)), 400)
+        self.tkImg = ImageTk.PhotoImage(self.pilImg.resize(size, Image.ANTIALIAS))
+
+    def contrast(self, coefficient):
+        result = Image.new('RGB', self.pilImg.size)
+
+        avg = 0
+        for x in range(self.pilImg.size[0]):
+            for y in range(self.pilImg.size[1]):
+                r, g, b = self.pilImg.getpixel((x, y))
+                avg += r * 0.299 + g * 0.587 + b * 0.114
+        avg /= self.pilImg.size[0] * self.pilImg.size[1]
+
+        palette = []
+        for i in range(256):
+            temp = int(avg + coefficient * (i - avg))
+            if temp < 0:
+                temp = 0
+            elif temp > 255:
+                temp = 255
+            palette.append(temp)
+
+        for x in range(self.pilImg.size[0]):
+            for y in range(self.pilImg.size[1]):
+                r, g, b = self.pilImg.getpixel((x, y))
+                result.putpixel((x, y), (palette[r], palette[g], palette[b]))
+        self.pilImg = result
+        if self.pilImg.size[0] >= self.pilImg.size[1]:
+            size = (700, int(self.pilImg.size[1] / (self.pilImg.size[0] / 700)))
+        else:
+            size = (int(self.pilImg.size[0] / (self.pilImg.size[1] / 400)), 400)
+        self.tkImg = ImageTk.PhotoImage(self.pilImg.resize(size, Image.ANTIALIAS))
+
+    def original(self):
+        self.pilImg = Image.open(self.path)
         if self.pilImg.size[0] >= self.pilImg.size[1]:
             size = (700, int(self.pilImg.size[1] / (self.pilImg.size[0] / 700)))
         else:
--- main.py
+++ main.py
@@ -21,9 +21,13 @@ class Main(tk.Frame):
 
         self.filemenu = tk.Menu(self.menu, tearoff=0)
         self.funcmenu = tk.Menu(self.menu, tearoff=0)
+        self.filtermenu = tk.Menu(self.menu, tearoff=0)
 
         self.menu.add_cascade(label='File', menu=self.filemenu)
         self.menu.add_cascade(label='Functions', menu=self.funcmenu)
+        self.menu.add_cascade(label='Filters', menu=self.filtermenu)
+        self.menu.add_cascade(label='Face detection', command=self.detection)
+        self.menu.add_cascade(label='To original', command=self.original)
 
         self.filemenu.add_cascade(label='Select', command=self.select)
         self.filemenu.add_cascade(label='Save', command=self.save)
@@ -35,6 +39,23 @@ class Main(tk.Frame):
 
         self.funcmenu.add_cascade(label='Resize', command=self.resize)
         self.funcmenu.add_cascade(label='Blur', command=self.blur)
+        self.funcmenu.add_cascade(label='Contour', command=self.contour)
+        self.funcmenu.entryconfigure(0, state=tk.DISABLED)
+        self.funcmenu.entryconfigure(1, state=tk.DISABLED)
+        self.funcmenu.entryconfigure(2, state=tk.DISABLED)
+
+        self.filtermenu.add_cascade(label='Brightness', command=self.brightness)
+        self.filtermenu.add_cascade(label='Negative', command=self.negative)
+        self.filtermenu.add_cascade(label='White-Black', command=self.white_black)
+        self.filtermenu.add_cascade(label='Grayscale', command=self.grayscale)
+        self.filtermenu.add_cascade(label='Sepia', command=self.sepia)
+        self.filtermenu.add_cascade(label='Contrast', command=self.contrast)
+        self.filtermenu.entryconfigure(0, state=tk.DISABLED)
+        self.filtermenu.entryconfigure(1, state=tk.DISABLED)
+        self.filtermenu.entryconfigure(2, state=tk.DISABLED)
+        self.filtermenu.entryconfigure(3, state=tk.DISABLED)
+        self.filtermenu.entryconfigure(4, state=tk.DISABLED)
+        self.filtermenu.entryconfigure(5, state=tk.DISABLED)
 
         tk.Frame(self.app, width=700, height=0).grid(row=0, column=0)
 
@@ -52,6 +73,15 @@ class Main(tk.Frame):
         self.target.grid(row=1, column=0)
         self.filemenu.entryconfigure(1, state=tk.NORMAL)
         self.filemenu.entryconfigure(2, state=tk.NORMAL)
+        self.funcmenu.entryconfigure(0, state=tk.NORMAL)
+        self.funcmenu.entryconfigure(1, state=tk.NORMAL)
+        self.funcmenu.entryconfigure(2, state=tk.NORMAL)
+        self.filtermenu.entryconfigure(0, state=tk.NORMAL)
+        self.filtermenu.entryconfigure(1, state=tk.NORMAL)
+        self.filtermenu.entryconfigure(2, state=tk.NORMAL)
+        self.filtermenu.entryconfigure(3, state=tk.NORMAL)
+        self.filtermenu.entryconfigure(4, state=tk.NORMAL)
+        self.filtermenu.entryconfigure(5, state=tk.NORMAL)
 
     def saveAs(self):
         self.convertor.saveAs(filedialog.asksaveasfilename(defaultextension='.png', initialdir="/", title="Select file", filetypes=(("jpeg files", "*.jpg"), ("png files", "*.png"))))
@@ -70,6 +100,39 @@ class Main(tk.Frame):
             self.target.destroy()
         self.target = tk.Label(self.app, text='TExt', image=self.convertor.tkImg)
         self.target.grid(row=1, column=0)
+
+    def detection(self):
+        self.convertor.detection()
+        self.updateImg()
+
+    def contour(self):
+        self.convertor.contour()
+        self.updateImg()
+
+    def brightness(self):
+        Brightness(self.convertor.brightness, self.updateImg)
+
+    def negative(self):
+        self.convertor.negative()
+        self.updateImg()
+
+    def white_black(self):
+        WhiteBlack(self.convertor.white_black, self.updateImg)
+
+    def grayscale(self):
+        self.convertor.grayscale()
+        self.updateImg()
+
+    def sepia(self):
+        self.convertor.sepia()
+        self.updateImg()
+
+    def contrast(self):
+        Contrast(self.convertor.contrast, self.updateImg)
+
+    def original(self):
+        self.convertor.original()
+        self.updateImg()
 
 
 class Resize(tk.Toplevel):
@@ -129,6 +192,90 @@ class Blur(tk.Toplevel):
 
     def resize(self):
         self.cb(int(self.x.get()))
+        self.upd()
+        self.destroy()
+
+
+class Brightness(tk.Toplevel):
+
+    def __init__(self, cb, update):
+        super().__init__(root)
+        self.title('Brightness')
+        self.resizable(False, False)
+
+        self.cb = cb
+        self.upd = update
+
+        tk.Frame(self, width=300).grid(row=0, column=1, columnspan=5)
+
+        tk.Label(self, text='Coefficient:').grid(row=1, column=1, padx=4, pady=4)
+        self.ent = tk.Entry(self)
+        self.ent.grid(row=1, column=2, columnspan=4, padx=4, pady=4)
+
+        tk.Button(self, text='Set', command=self.resize).grid(row=3, column=4, padx=4, pady=4)
+        tk.Button(self, text='Close', command=self.destroy).grid(row=3, column=5, padx=4)
+
+        self.grab_set()
+        self.focus_set()
+
+    def resize(self):
+        self.cb(float(self.ent.get()))
+        self.upd()
+        self.destroy()
+
+
+class WhiteBlack(tk.Toplevel):
+
+    def __init__(self, cb, update):
+        super().__init__(root)
+        self.title('White-Black')
+        self.resizable(False, False)
+
+        self.cb = cb
+        self.upd = update
+
+        tk.Frame(self, width=300).grid(row=0, column=1, columnspan=5)
+
+        tk.Label(self, text='Coefficient:').grid(row=1, column=1, padx=4, pady=4)
+        self.ent = tk.Entry(self)
+        self.ent.grid(row=1, column=2, columnspan=4, padx=4, pady=4)
+
+        tk.Button(self, text='Set', command=self.resize).grid(row=3, column=4, padx=4, pady=4)
+        tk.Button(self, text='Close', command=self.destroy).grid(row=3, column=5, padx=4)
+
+        self.grab_set()
+        self.focus_set()
+
+    def resize(self):
+        self.cb(float(self.ent.get()))
+        self.upd()
+        self.destroy()
+
+
+class Contrast(tk.Toplevel):
+
+    def __init__(self, cb, update):
+        super().__init__(root)
+        self.title('Contrast')
+        self.resizable(False, False)
+
+        self.cb = cb
+        self.upd = update
+
+        tk.Frame(self, width=300).grid(row=0, column=1, columnspan=5)
+
+        tk.Label(self, text='Coefficient:').grid(row=1, column=1, padx=4, pady=4)
+        self.ent = tk.Entry(self)
+        self.ent.grid(row=1, column=2, columnspan=4, padx=4, pady=4)
+
+        tk.Button(self, text='Set', command=self.resize).grid(row=3, column=4, padx=4, pady=4)
+        tk.Button(self, text='Close', command=self.destroy).grid(row=3, column=5, padx=4)
+
+        self.grab_set()
+        self.focus_set()
+
+    def resize(self):
+        self.cb(float(self.ent.get()))
         self.upd()
         self.destroy()
 
